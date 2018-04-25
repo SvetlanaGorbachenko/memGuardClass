192e8e41a345275f3e57f4c946a38a63d0c0c3b7
Svetlana Gorbachenko <svetlanagorbachenko@ukr.net>
2018-04-25 10:27:00 +0300

    Add files via upload

---
+++ Header.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+template <typename T>
+class memGuardClass
+{
+	T *m_pData;
+	bool m_bArray;
+public:
+	memGuardClass(T *pData = 0, bool bArray = false): m_pData(pData), m_bArray(bArray)
+	{
+		cout << "constructor memGuardClass with parameters: m_pData = " << m_pData << " m_bArray = " << m_bArray << " this: " << this << endl;
+	}
+	~memGuardClass()
+	{
+		cout << "destructor ~memGuardClass()" << endl;
+
+		if (m_bArray != false)
+		{
+			delete[] m_pData;
+		}
+		else
+		{
+			delete m_pData;
+		}
+	}
+};
